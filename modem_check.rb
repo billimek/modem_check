f8b33c6ee6043154a473151ed318d1f49c9091de
Jeff Billimek <jeff@billimek.com>
2014-08-08 10:52:09 -0400

    adding comments describing the purpose and function of the script

--- modem_check.rb
+++ modem_check.rb
@@ -1,5 +1,18 @@
 #!/opt/local/bin/ruby
 
+# The purpose of this script is to perform a health-check of the cable modem and connection.
+# This is specifically intended to be used with the Motorola SB6120 cable modem
+# It will collect all of the downstream & upstream signal-to-noise and power level data
+# as well as the most recent log timestamp and entry.
+# It also perfoms a ping test (currently 100 poings to www.comcast.net) to determine average and max ping time as well as packet loss
+# all of the data is printed in a comma-seperated format for use in a spreadsheet to analyze patterns
+# the output is in the following format:
+#timestamp,% packet loss,avg ping time(ms),max ping time(ms),down channel 1,snr,power,down channel 2,snr,power,down channel 3,snr,power,down channel 4,snr,power,up channel 1,power,up channel 2,power,up channel 3,power,log timestamp,log message
+
+# The script is intended to be run on cron (I run it every 5 minutes)
+# here is an example crontab entry:
+# */5 * * * *       $HOME/Documents/git/modem_check/modem_check.rb >> $HOME/Documents/git/modem_check/modem_log.txt 2>&1
+
 # nokogiri is our scraping/parsing library
 # you will need to install it with "gem install nokogiri"
 require 'nokogiri'
